f685ade6de4477afa3b7e9bde343db8376ec33c6
Havva Odabaşı <appswift.developer@gmail.com>
2026-01-06 12:56:03 +0300

    Initial Commit

---
+++ Calculator/CalculatorApp.swift
@@ -0,0 +1,17 @@
+//
+//  CalculatorApp.swift
+//  Calculator
+//
+//  Created by Havva Odabaşı on 6.01.2026.
+//
+
+import SwiftUI
+
+@main
+struct CalculatorApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
